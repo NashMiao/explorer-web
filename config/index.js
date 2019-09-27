fe385a5fb39f5e1b37857355ef1c086f80cd020b
NashMiao <wdx7266@vip.qq.com>
2019-09-27 16:34:48 +0800

    update routes

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://nashmiao.github.io/explorer-web/',
+    assetsPublicPath: '/explorer-web/',
 
     /**
      * Source Maps
